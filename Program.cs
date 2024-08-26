205ca3fa41a5a7c18469abf50bb801f78ae7e68b
TranLeHieuGiang <giangtran.31231027369@st.ueh.edu.vn>
2024-08-26 16:06:15 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,59 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 using System;
-using System.ComponentModel.DataAnnotations;
-using System.Security.Cryptography;
-using System.Xml;
+using System.ComponentModel.Design;
 internal class Program
 {
     private static void Main(string[] args)
     {
-        bt01();
-        bt02();
-        bt03();
-        bt04();
-        bt05();
-    }
-    static void bt01()
-    {
-        Console.Write("Nhap vao do C:");
-        float c = Convert.ToSingle(Console.ReadLine());
-        float doF = (c * 18 / 10) + 32;
-        float doK = c + 273;
-        Console.WriteLine($"Do {c} tuong duong voi {doK} do F va {doF} do F");
-    }
-    static void bt02()
-    {
-        Console.Write("Nhap ban kinh hinh cau:");
-        float r = Convert.ToSingle(Console.ReadLine());
-        double pi = Math.PI;
-        double surface = 4* pi*Math.Pow(pi, 2);
-        double volume = 4/3 * pi * Math.Pow(pi, 3);
-        Console.WriteLine($"Surface va Volume cua hinh cau lan luot la {surface} va {volume}");
-    }
-    static void bt03()
-    {
-        Console.Write("Nhap so 1 vao:");
-        int a1 = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Nhap so 2 vao:");
-        int a2 = Convert.ToInt32(Console.ReadLine());
-        int tong = a1 + a2;
-        int hieu = a1 - a2;
-        int tich = a1 * a2;
-        int thuong = a1 / a2;
-        Console.WriteLine($"cac phep tinh la: {tong},{hieu},{tich},{thuong}");
-    }
-    static void bt04()
-    {
-        Console.Write("Nhap vao chuoi sau: ");
-        string text = Console.ReadLine();
-        Console.WriteLine(text);
-    }
-    static void bt05()
-    {
-        Console.Write("Nhap vao chuoi sau");
-        string text = Convert.ToString(Console.ReadLine());
-        int length = text.Length;
-        Console.WriteLine($"Do dai chuoi la: {length}");
-    }
+        //Nguoi choi co tien;
+        Console.WriteLine("Nguoi choi co 1000$ lun nhoe");
+        do
+        {
+            //Nguoi choi bat dau choi;
+            Random rnd = new Random();
+            int com_num = rnd.Next(1, 100);
+            //Console.WriteLine(com_num);
+            int man_num = 0;
 
+            for (int i = 0; i < 10; i++)
+            {
+                    Console.Write("Nhap so doan: ");
+                    man_num = Convert.ToInt32(Console.ReadLine());
+                    if (man_num == com_num)
+                    {
+                        Console.Write("Gioi qua gioi qua ~~");
+                        break;
+                    }
+                    else
+                       if (man_num < com_num)
+                        Console.WriteLine("So cua ban nho hon so voi dap an. ");
+                    else Console.WriteLine("So cua ban lon hon so voi dap an. ");
+            }
+            if (man_num != com_num) 
+            Console.WriteLine($"So cua ban la {man_num} nhung dap an la {com_num}. Rat tiec huhu :((( ");
+            
+            Console.WriteLine("Ban co con mun tip tuc cung chung toi? Y/N");
+            Console.WriteLine();
+            string trl = "" + Console.ReadLine();
+            if (trl.ToUpper().Equals(("Y"))) ;
+            {  
+                int solan = Convert.ToInt32(trl);
+                solan++;
+                Console.WriteLine("Ban da nap them 50$ de choi tiep ");
+                break;
+            }
+            
+            //Console.WriteLine("Ban da choi va con lai ");
+            //Console.WriteLine("Hen gap lai ^^ ");
+        } while (true);    
+    }
+    
 }
