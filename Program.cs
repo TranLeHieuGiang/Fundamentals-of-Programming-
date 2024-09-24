6817e70cc27dc6784c7de5c8592b753a86a70d84
TranLeHieuGiang <giangtran.31231027369@st.ueh.edu.vn>
2024-09-24 16:33:02 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,100 +1,92 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp3
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        private void Main(string[] args)
+        Console.Write("Nhap vao chuoi sau: ");
+        string[] s = new string[100];
+        bt01(s);
+        bt02(s);
+        bt03(s);
+        bt04(s);
+        bt05(s);
+        bt06(s);
+        bt07(s);
+    }
+    static void bt01(string[] s)
+    {
+        
+        Console.WriteLine(s);
+    }
+    static void bt02(string[] s)
+    {
+        
+        int count = 0;
+        
         {
-            bt01();
-            bt02();
-            bt03();
+            count = 1;
         }
-
-        static void bt01()
+        while (count <s.Length)
         {
-            int m = max(1);
-            m = max(1, 2);
-            Console.WriteLine(m);
-            Console.ReadKey();
-        }
-        static int max(int a, params int[] args)
-        {
-            if (args.Length == 0)
-            {
-                return a;
-            }
-            int m = args[0];
-
-            foreach (int i in args)
-            {
-                if (i > m)
-                    m = i;
-            }
-            return Math.Max(m, a);
-        }
-        static void bt02()
-        {
-            long kq = factorial();
-            Console.WriteLine(kq);
-        }
-        static long factorial(int n)
-        {
-            long f = 1;
-            for (int i = 1; i <= n; i++)
-                f += i;
-            return f;
-        }
-        static long factorial_recursion(int n)
-        {
-            if (n == 0) return 1;
-            return n * factorial_recursion(n - 1);
-        }
-
-        static void bt03()
-        {
-
-            static bool Prime(int number)
-            {
-                for (int i = 2; i <= number / 2; i++) ;
-                if (number % 1 == 0)
-                    return false;
-            }
-            static void PrimeuderN(int n)
-            {
-                for (int i = 1; i <= n; i++)
-                    if (Prime(i))
-                        Console.WriteLine(i);
-            }
            
-            static void PrimeNnumber(int n)
-            
-            {
-                int count = 0;
-                int number = 1;
-                while (count <= n)
-                {
-                    if(Prime(number))
-                    {
-                        Console.WriteLine(number);
-                        count++;
-                    }
-                }
-            }
+        }
+        {
 
-
-
+            count++;
         }
 
+        Console.WriteLine($"Do dai chuoi la: {count}");
+    }
+    static void bt03(string[] s)
+    {
+        foreach (string v in s)
+        {
+            Console.Write($" {v} ");
+        }
+        Console.WriteLine();
+    }
+    static void bt04(string[] s)
+    {
+       
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            Console.Write(s[i] + " "); 
+        }
+        Console.WriteLine();
+    }
+    static void bt05(string[] s)    
+    {
+        
+    }
+   static void bt06(string[] s)
+    {
+        Console.Write("Nhap vao chuoi 1: ");
+        string s3 = Console.ReadLine();
 
+        if (s3.Length > s.Length)
+            Console.WriteLine($"Chuoi dai hon la: {s3}");
+        else Console.WriteLine($"Chuoi dai hon la: {s}");
+    }
+    static int bt07(string[] s)
+    {
+        int[] a = new int[3];
+        int c = 0, v = 0, d = 0;
 
+        foreach (int i in a)
+        {
+            if ((i >= 'a' && i <= 'z') || (c >= 'A' && c <= 'Z'))
 
+            {
+                c++;
+            }
+            if (c >= 0 && c <= 9)
+                v++;
+            else d++;
+        }
+        a[0] = c;
+        a[1] = v;
+        a[2] = d;
+        return 0;
+        Console.WriteLine();
     }
 }
-
